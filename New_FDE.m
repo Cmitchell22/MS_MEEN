c68e8c5e5ab932b38351f5a883383f7778d0cca7
Mitchell <cmitchell22@TAMU.EDU>
2017-11-07 12:18:38 -0600

    Making modifications to the Method. Need to figure out the issue with the final y_corrector when it is added to the array. Its failing at the moment. The B-Coefficients are functional, and the Summation has been fixed to be bypassed on the first go, as it should.

--- New_FDE.m
+++ New_FDE.m
@@ -1,57 +1,84 @@
+clc;
+clear all;
 syms t;
 fdefun = t^2;
 alpha  = 0.5;
 b0 = -1;
 b1 = alpha + 2;
 y_lag_sum = 0; 
-y_corrector_j = fdefun;
 m = 1;
 time_n = [0:0.1:1];
 first_pass = true;
-h = 0.1;
+h = 0.05;
+y_corrector(1) = 0;
 
+ii = 1;
+jj = 1;
 
-for n = 1:11
+% Indexing is set up as follows:
+% [ t = 0, t = 0.05 , t = 0.1, t = 0.15 ...]
+% For n = 0, 1st pass:
+% [ t = n, t = n+h ...]
+% For n = 0.05=h, 1st pass:
+% [ t = n-h, t = n, t = n+h]
+
+% n is the current time step. Starts at zero, and increases by h each time.
+for n = 0:h:h
+    n
     
-      for j = 1:n+1-1
-      
+      ii = 1;
+      y_lag_sum = 0;
+      for j = 0:h:n-h
+      j
         fprintf('making a pass\n');
         
-        [B_0n_Coeff, B_1n_Coeff] = B_Coefficients((j-1)*h,(j+1-1)*h,(n+1-1)*h, alpha)
+        %Calculate Coefficients for lag terms: CONFIRMED GOOD
+        [B_0j_Coeff, B_1j_Coeff] = B_Coefficients(j,j+h, n+h, alpha)
         
-        f_corrector_j = Take_Caputo_Deriv(y_corrector_j, alpha, m)
+%         % Take Caputo Derivative to get f_tilde(t_j) from y_tilde(t_j)
+%         f_corrector_j(ii) = Take_Caputo_Deriv(y_corrector(ii), alpha, m)
+%         
+%         % Take Caputo Derivative to get f_tilde(t_j+1) from y_tilde(t_j+1)
+%         f_corrector_jj(ii+1) = Take_Caputo_Deriv(y_corrector(ii+1), alpha, m)
         
-        f_corrector_jj = Take_Caputo_Deriv(y_corrector_jj, alpha, m)
+        % Summation Portion of the lag term. 
+        y_lag_sum = y_lag_sum + B_0j_Coeff*f_corrector(ii) + B_1j_Coeff*f_corrector(ii+1)
         
-        y_lag_sum = y_lag_sum + B_0n_Coeff*f_corrector_j + B_1n_Coeff*f_corrector_jj
+        %Increment the interior counter
+        ii = ii + 1;
       end 
       
-    g = calculate_IC(fdefun, alpha, n+1)
+    %Calculate the initial condition term **Still unsure about this term**  
+    g(jj+1) = calculate_IC(fdefun, alpha, n+h)
     
-    y_lag(n+1) = 1/gamma(alpha)*y_lag_sum
+    % Lag term for this time step is now complete!
+    y_lag_final(jj+1) = 1/gamma(alpha)*y_lag_sum
     
-    % Predictor term at the new step
-    if n > 1
-      y_predictor(n+1) = g + y_lag(n+1) + h^alpha/gamma(alpha+2)*(b0*f_corrector(n-1) + b1*f_corrector(n))
+    %Now we are able to calculate the predictor:
+    
+    % Take Caputo Derivatives of the y_corrector term at current step
+    f_corrector(jj) = Take_Caputo_Deriv(y_corrector(jj), alpha, m)
+    
+    % Predictor term at the new step (Need to have n-1 term. First pass: n = 0, n+1 = n+h, n-1 is not bound.)
+    if jj > 1
+        y_predictor(jj+1) = g(jj+1) + y_lag_final(jj+1) + h^alpha/gamma(alpha+2)*(b0*f_corrector(jj-1) + b1*f_corrector(jj))
     else
-        error('n is an indexing term; it cannot be <= 0, as Matlab is a 1-indexed program.')
+        y_predictor(jj+1) = g(jj+1) + y_lag_final(jj+1) + h^alpha/gamma(alpha+2)*(b0*0 + b1*f_corrector(jj))
     end
+    % take Caputo Derivative of the predictor
+    f_predictor(jj+1) = Take_Caputo_Deriv(y_predictor(jj+1), alpha, m)
     
-    f_predictor(n+1) = Take_Caputo_Deriv(y_predictor(n+1), alpha, m)
-    
-    f_corrector(n) = Take_Caputo_Deriv(y_corrector(n), alpha, m)
-    
-    if first_pass == false
-        f_predictor(n+1) = Take_Caputo_Deriv(y_predictor(n+1), alpha, m)
-    end
-    
-    [B_0n_Coeff, B_1n_Coeff] = B_Coefficients((n-1)*h,(n+1-1)*h, (n+1-1)*h, alpha)
+    %Calculate Coefficients for
+    [B_0n_Coeff, B_1n_Coeff] = B_Coefficients(n,n+h, n+h, alpha)
     
     %Increment term at the new step
-    y_increment(n+1) = 1/gamma(alpha)*(B_0n_Coeff*f_corrector(n) + B_1n_Coeff*f_predictor(n+1))
+    y_increment(jj+1) = 1/gamma(alpha)*(B_0n_Coeff*f_corrector(jj) + B_1n_Coeff*f_predictor(jj+1))
 
     %Corrector term at the new step
-    y_corrector(n + 1) = g + y_lag(n + 1) + y_increment(n + 1)
+    y_corrector(jj+1) = g(jj+1) + y_lag_final(jj+1) + y_increment(jj+1)
+    
+    f_corrector(jj+1) = Take_Caputo_Deriv(y_corrector(jj+1), alpha, m)
+    
+    jj = jj + 1
      
-    first_pass = false;
 end
--- calculate_IC.m
+++ calculate_IC.m
@@ -4,7 +4,7 @@ m = round(alpha, 0);
 g_sum = 0;
 
 for k = 0:m
-    g_sum = g_sum + Take_Deriv(fdefun,k)*(t_eval)^(k)/factorial(k) %***************
+    g_sum = g_sum + Take_Deriv(fdefun,k)*(t_eval)^(k)/factorial(k); %***************
 end
 
 g = subs(g_sum, t_eval);
