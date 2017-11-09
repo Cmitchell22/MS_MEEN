84d1b0f8de6987426647d3814dc1a0b4908db1d6
Mitchell <cmitchell22@TAMU.EDU>
2017-11-09 11:47:26 -0600

    Adding minor changes. Still having assignment issue with empty sym

--- A_Coefficients.m
+++ A_Coefficients.m
@@ -1,7 +1,7 @@
 function [A_0n_Coeff, A_1n_Coeff, A_2n_Coeff] = A_Coefficients(integrand_low,integrand_high,t_low,t_mid,t_up,t_eval,alpha)
  
  syms tau
- [Q_low, Q_mid, Q_up] = Quadratic_Interp(t_low, t_mid, t_up)
+ [Q_low, Q_mid, Q_up] = Quadratic_Interp(t_low, t_mid, t_up);
  A_0n_funct = ((t_eval-tau)^(alpha-1)) * Q_low;
  A_0n_Coeff = int(A_0n_funct,integrand_low,integrand_high);
 
